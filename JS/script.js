be9d431409d63a57e1c9d67dc715f8fe69a5930d
Nikita <kkrasn2016@yandex.ru>
2022-11-16 23:49:34 +0300

    Add end of the game and the restart button

--- JS/script.js
+++ JS/script.js
@@ -5,13 +5,14 @@ let message = document.getElementById('message');
 let rock = document.getElementsByClassName('card')[0];
 let paper = document.getElementsByClassName('card')[1];
 let scissors = document.getElementsByClassName('card')[2];
+let playAgainButton = document.getElementById('play-again');
 
 let idleTime; //if the user haven't clicked anything within 1 sec clear the scoreboard message
 
 rock.addEventListener('click', () => playRound('rock'));
 paper.addEventListener('click', () => playRound('paper'));
 scissors.addEventListener('click', () => playRound('scissors'));
-
+playAgainButton.addEventListener('click',() => restart());
 
 
 function updatePlayerScore() {
@@ -49,14 +50,36 @@ function showScoreboardMessage(playerSelection,machineSelection,playerWin) {
     }
 }
 
+function gameOver(playerWin) {
+    if(playerWin){
+        message.textContent = 'You win';
+    }
+    else{
+        message.textContent = 'You lose';
+    }
+    rock.style.display = 'none';
+    paper.style.display = 'none';
+    scissors.style.display = 'none';
+    playAgainButton.style.display = 'block';
+    clearTimeout(idleTime);
+    document.onclick = null; // If user clicks it doesn't removes the scoreboard message
+}
 
+function restart() {
+    rock.style.display = '';
+    paper.style.display = '';
+    scissors.style.display = '';
+    playAgainButton.style.display = 'none';
+    machineScore.textContent = 0;
+    playerScore.textContent = 0;
+}
 
 //Game
 function checkScore() {
     if(playerScore.textContent >= 5){
-
+        gameOver(true)
     }else if(machineScore.textContent >= 5){
-        
+        gameOver(false)
     }
 }
 
@@ -70,15 +93,15 @@ function playRound(playerSelection){
         case playerSelection === 'rock' && machineSelection === 'scissors':
         case playerSelection === 'paper' && machineSelection === 'rock':
         case playerSelection === 'scissors' && machineSelection === 'paper':
-            updatePlayerScore();
             showScoreboardMessage(playerSelection, machineSelection, true);
+            updatePlayerScore();
             break;
         
         case playerSelection === 'scissors' && machineSelection === 'rock':
         case playerSelection === 'rock' && machineSelection === 'paper':
         case playerSelection === 'paper' && machineSelection === 'scissors':
-            updateMachineScore();
             showScoreboardMessage(playerSelection, machineSelection, false);
+            updateMachineScore();
             break;
         default:
             showScoreboardMessage(playerSelection, machineSelection);
