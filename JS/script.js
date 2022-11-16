d5c8c193c9094ed19d0405432f7aadab863ad8df
Nikita <kkrasn2016@yandex.ru>
2022-11-16 19:54:54 +0300

    Hook up the initial script to the site

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+let playerScore = document.getElementById('player-score')
+let machineScore = document.getElementById('machine-score')
+let winner = document.getElementById('winner')
+let playerCardChoice = Object.values(document.getElementsByClassName('card'));
+
+playerCardChoice.forEach(e => {
+    
+});
+
+function getComputerChoice() {
+    //Machine returns choice: rock paper sissors
+}
+
+function playRound(player, computer){
+    return "You Lose! " + 'somethings'
+}
+
+function getPlayerChoice(){
+
+}
+
+function game(){
+    for (let i = 0; i < array.length; i++) {
+        playRound();
+    }
+}
